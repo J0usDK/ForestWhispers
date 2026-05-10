03478ae7b9dc79458e2919702f52e172f3188821
JDK <dimakurapovi@gmail.com>
2026-05-11 01:09:30 +0200

    basic first person movement and camera

--- Code/Components/Player.h
+++ Code/Components/Player.h
@@ -1,95 +1,54 @@
 // Copyright 2017-2020 Crytek GmbH / Crytek Group. All rights reserved.
 #pragma once
 
-#include <CryEntitySystem/IEntityComponent.h>
-#include <CryMath/Cry_Camera.h>
-
-#include <ICryMannequin.h>
-#include <CrySchematyc/Utils/EnumFlags.h>
-
 #include <DefaultComponents/Cameras/CameraComponent.h>
 #include <DefaultComponents/Input/InputComponent.h>
-#include <DefaultComponents/Audio/ListenerComponent.h>
+#include <DefaultComponents/Physics/CharacterControllerComponent.h>
+#include <DefaultComponents/Geometry/AdvancedAnimationComponent.h>
 
 ////////////////////////////////////////////////////////
 // Represents a player participating in gameplay
 ////////////////////////////////////////////////////////
 class CPlayerComponent final : public IEntityComponent
-{
-	enum class EInputFlagType
-	{
-		Hold = 0,
-		Toggle
-	};
-
-	enum class EInputFlag : uint8
-	{
-		MoveLeft = 1 << 0,
-		MoveRight = 1 << 1,
-		MoveForward = 1 << 2,
-		MoveBack = 1 << 3
-	};
-
-	static constexpr EEntityAspects InputAspect = eEA_GameClientD;
-	
+{	
 public:
 	CPlayerComponent() = default;
 	virtual ~CPlayerComponent() = default;
 
-	// IEntityComponent
 	virtual void Initialize() override;
-
 	virtual Cry::Entity::EventFlags GetEventMask() const override;
 	virtual void ProcessEvent(const SEntityEvent& event) override;
-	
-	virtual bool NetSerialize(TSerialize ser, EEntityAspects aspect, uint8 profile, int flags) override;
-	virtual NetworkAspectType GetNetSerializeAspectMask() const override { return InputAspect; }
-	// ~IEntityComponent
 
 	// Reflect type to set a unique identifier for this component
 	static void ReflectType(Schematyc::CTypeDesc<CPlayerComponent>& desc)
 	{
 		desc.SetGUID("{63F4C0C6-32AF-4ACB-8FB0-57D45DD14725}"_cry_guid);
+		desc.AddMember(&CPlayerComponent::m_movementSpeed, 'pms', "playermovementspeed", "Player Movement Speed", "The speed at which the player moves", 0.0f);
+		desc.AddMember(&CPlayerComponent::m_rotationSpeed, 'prs', "playerrotationspeed", "Player Rotation Speed", "The speed at which the player rotates", 0.0f);
+		desc.AddMember(&CPlayerComponent::m_defaultCameraPos, 'dcp', "defaultcamerapos", "Default Camera Position", "The default position of the camera", Vec3(0.0f, 0.0f, 0.0f));
+		desc.AddMember(&CPlayerComponent::m_rotationLimitsMinPitch, 'cpmi', "rotationlimitsminpitch", "Rotation Limits Min Pitch", "Minimum pitch rotation limit for the player camera", 0.0f);
+		desc.AddMember(&CPlayerComponent::m_rotationLimitsMaxPitch, 'cpma', "rotationlimitsmaxpitch", "Rotation Limits Max Pitch", "Maximum pitch rotation limit for the player camera", 0.0f);
 	}
 
-	void OnReadyForGameplayOnServer();
-	bool IsLocalClient() const { return (m_pEntity->GetFlags() & ENTITY_FLAG_LOCAL_PLAYER) != 0; }
-	
-protected:
-	void Revive(const Matrix34& transform);
-	void HandleInputFlagChange(CEnumFlags<EInputFlag> flags, CEnumFlags<EActionActivationMode> activationMode, EInputFlagType type = EInputFlagType::Hold);
-
-	// Called when this entity becomes the local player, to create client specific setup such as the Camera
-	void InitializeLocalPlayer();
-	
-	// Start remote method declarations
-protected:
-	// Parameters to be passed to the RemoteReviveOnClient function
-	struct RemoteReviveParams
-	{
-		// Called once on the server to serialize data to the other clients
-		// Then called once on the other side to deserialize
-		void SerializeWith(TSerialize ser)
-		{
-			// Serialize the position with the 'wrld' compression policy
-			ser.Value("pos", position, 'wrld');
-			// Serialize the rotation with the 'ori0' compression policy
-			ser.Value("rot", rotation, 'ori0');
-		}
-		
-		Vec3 position;
-		Quat rotation;
-	};
-	// Remote method intended to be called on all remote clients when a player spawns on the server
-	bool RemoteReviveOnClient(RemoteReviveParams&& params, INetChannel* pNetChannel);
-	
-protected:
-	bool m_isAlive = false;
-
+private:
+	//Camera Component
 	Cry::DefaultComponents::CCameraComponent* m_pCameraComponent = nullptr;
 	Cry::DefaultComponents::CInputComponent* m_pInputComponent = nullptr;
-	Cry::Audio::DefaultComponents::CListenerComponent* m_pAudioListenerComponent = nullptr;
+	Cry::DefaultComponents::CCharacterControllerComponent* m_pCharacterController = nullptr;
+	Cry::DefaultComponents::CAdvancedAnimationComponent* m_pAdvancedAnimationComponent = nullptr;
 
-	CEnumFlags<EInputFlag> m_inputFlags;
+	void InitializeInput();
+	void PlayerMovement();
+	void PlayerRotation();
+
+	Quat m_lookOrientation;
+	Vec3 m_defaultCameraPos;
+
+	Vec2 m_movementDelta;
 	Vec2 m_mouseDeltaRotation;
+
+	float m_movementSpeed;
+	float m_rotationSpeed;
+	float m_rotationLimitsMinPitch;
+	float m_rotationLimitsMaxPitch;
 };
